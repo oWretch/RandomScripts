78c635969aac60a2cb39e68a07d47c1fa815972b
Jerome Brown <Jerome@DailyFuneral.net>
2020-05-12 13:18:01 +1000

    Update batch-convert-videos

--- batch-convert-videos.sh
+++ batch-convert-videos.sh
@@ -1,6 +1,7 @@
 find . -type f -name "*.mkv" -exec sh -c '
   for mkv do 
-    if [ ! -f "${mkv%.mkv}.m4v"]
-    transcode-video --m4v --crop detect --fallback-crop minimal --encoder vt_h264 "$mkv"
+    if [ ! -f "${mkv%.mkv}.m4v" ]; then
+      transcode-video --m4v --crop detect --fallback-crop minimal --encoder vt_h264 "$mkv"
+    fi
   done
 ' sh {} +

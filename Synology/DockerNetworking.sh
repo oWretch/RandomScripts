515aefb4a10091d932f0d8c92f50dd743eef61bb
Jerome Brown <Jerome@DailyFuneral.net>
2020-12-11 12:28:01 +1100

    Remove VLAN tag from default network

--- Synology/DockerNetworking.sh
+++ Synology/DockerNetworking.sh
@@ -6,7 +6,7 @@ docker network create \
         --gateway=172.22.0.1 \
         --subnet=172.22.0.0/24 \
         --ip-range=172.22.0.224/27 \
-        -o parent=bond0.1 \
+        -o parent=bond0 \
         macvlan-1
 
 # VLAN 13

926fcb2d2b6aa2b77455260f28179611d5faba3e
Jerome Brown <Jerome@DailyFuneral.net>
2021-03-24 15:29:38 +1100

    Whitespace cleanup

--- Synology/Portainer.sh
+++ Synology/Portainer.sh
@@ -1,18 +1,18 @@
 # Portainer-CE 2.0 Server
- docker run \
- 	--name=portainer \
+docker run \
+    --name=portainer \
     --hostname portainer \
-	--net macvlan1 \
-	--ip 172.22.0.251 \
-	--dns 172.22.0.1 \
+    --net macvlan1 \
+    --ip 172.22.0.251 \
+    --dns 172.22.0.1 \
     --detach \
- 	--restart=always \
+    --restart=always \
     --env EULA=TRUE \
- 	portainer/portainer-ce \
- 	--bind :443 \
- 	--ssl \
-	--volume /volume2/docker/portainer/data:/data \
- 	--volume /volume2/docker/portainer/certs:/certs:ro \
-	--volume /var/run/docker.sock:/var/run/docker.sock \
-	--sslcert /certs/fullchain.pem \
-	--sslkey /certs/privkey.pem
+    --volume /volume2/docker/portainer/data:/data \
+    --volume /volume2/docker/portainer/certs:/certs:ro \
+    --volume /var/run/docker.sock:/var/run/docker.sock \
+    portainer/portainer-ce \
+    --bind :443 \
+    --ssl \
+    --sslcert /certs/fullchain.pem \
+    --sslkey /certs/privkey.pem
--- Synology/UniFiController.sh
+++ Synology/UniFiController.sh
@@ -1,12 +1,13 @@
 # UniFi Controller
- docker run \
- 	--name=UniFi-Controller \
+docker run \
+    --name=UniFi-Controller \
     --hostname unifi \
+    --user unifi \
     --net macvlan1 \
     --ip 172.22.0.224 \
     --dns 172.22.0.1 \
     --detach \
     --restart=always \
     --env TZ='Australia/Melbourne' \
- 	jacobalberty/unifi:latest
     --volume /volume2/docker/unifi:/unifi \
+    jacobalberty/unifi:latest
